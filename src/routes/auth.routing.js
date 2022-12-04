0a4dbb1d6136a1648543ea02b0ff9bf85338d1fc
DESKTOP-26LTFUC\VinhNgo <vinhtrungngo1907@gmail.com>
2022-12-04 15:13:51 +0700

    second commit

--- src/routes/auth.routing.js
+++ src/routes/auth.routing.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authCtrl = require("../controllers/auth.controller");
 
-
-
 // @route POST api/auth/register
 // @desc Register user
 // @access Public
@@ -13,5 +11,15 @@ router.post('/register',authCtrl.register);
 // @desc Login user
 // @access Public
 router.post('/login', authCtrl.login);
+
+// @route POST api/auth/activate-account
+// @desc Activate account
+// @access Public
+router.post('/activate-account', authCtrl.activeAccount);
+
+// @route POST api/auth/refresh-token
+// @desc Refresh token
+// @access Public
+router.post('/refresh-token', authCtrl.refreshToken);
 
 module.exports = router
