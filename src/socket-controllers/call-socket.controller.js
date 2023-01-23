9a7b7c5b37a670879da13470bc4c7ee4b027e6ee
DESKTOP-3VG7I1R\Vinh Ngo <vinhtrungngo1907@gmail.com>
2023-01-23 13:44:25 +0700

    [socket] - call user

--- src/socket-controllers/call-socket.controller.js
+++ src/socket-controllers/call-socket.controller.js
@@ -4,8 +4,31 @@ const editData = (data, id, call) => {
 }
 
 const callSocketController = {
-    callUser: async (io, socket, users, sender, recipient) => {
+    callUser: async (io, socket, users, data) => {
         // users.push({ id: user._id, socketId: socket.id, followers: user.followers });
+        try {
+            users = editData(users, data.sender, data.recipient);
+            const client = users.find(u => u.id === data.recipient);
+            if(client){
+                if(client.call){
+                    socket.emit('userBusy', data);
+                    users = editData(users, data.sender, null);
+                    
+                }else{
+                    users = editData(users, data.recipient, data.sender);
+                    socket.to(`${client.socketId}`).emit('callUserToClient', data);
+                }
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    endCall: async (io, socket, users, data) => {
+        try {
+
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
 
