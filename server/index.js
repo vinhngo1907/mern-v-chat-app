2bae9ad8fe58fc11b01a123b750dbe1e4503a832
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-06-24 10:28:37 +0700

    update

--- server/index.js
+++ server/index.js
@@ -82,9 +82,9 @@ io.on("connection", socket => {
 ExpressPeerServer(http, { path: '/' });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static('../client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
     })
 }
 
