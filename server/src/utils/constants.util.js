a9c90311c4d2d839f239a49c7c1623ea056908af
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-07-08 10:27:17 +0700

    commit

--- server/src/utils/constants.util.js
+++ server/src/utils/constants.util.js
@@ -1,3 +1,20 @@
 exports.clientUrl = process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : 'https://mern-v-chat-app.netlify.app'
+    : 'https://mern-v-chat-app.netlify.app';
+
+
+exports.cookieOptions = process.env.NODE_ENV !== "production"
+    ? {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'Lax',
+        path: '/',
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    }
+    : {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        path: '/',
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    };
